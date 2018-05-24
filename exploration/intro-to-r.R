6db58c15e74bc1e0f4155b0ce8cf8e7adb22f40d
Michael Uftring <michael.uftring@gmail.com>
2018-05-24 10:30:17 -0400

    Week 2: intro to R (script, notebook)

---
+++ exploration/intro-to-r.R
@@ -0,0 +1,11 @@
+# to run this, do one of the following:
+# 1. highlight, and click: Run
+# 2. press: Control-Enter
+a <- 3+3
+b <- 4+4
+
+# list the built-in data sets available in R
+data()
+
+# view a data set
+View(mtcars)
