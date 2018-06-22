6454499d8d6a7ed4f1927a6c221eda4fea775496
Michael Uftring <michael.uftring@gmail.com>
2018-06-22 12:48:08 -0400

    remove video files (prep for Git LFS)

--- week-04/r-notes.md
+++
@@ -1,1 +0,0 @@
-# Dataset Subsets in R
--- week-07/Dependent Sample T-Test.r
+++
@@ -1,8 +0,0 @@
-library(car)
-View(Davis)
-help(Davis)
-
-t.test(Davis$repwt,Davis$weight,paired=T)
-t.test(Davis$repwt,Davis$weight,paired=T,conf.level=0.99)
-t.test(Davis$repwt,Davis$weight,paired=T,alternative="greater")
-t.test(Davis$repwt,Davis$weight,paired=T,alternative="less")
