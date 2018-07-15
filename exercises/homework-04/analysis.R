cd444184491319732433ab76e7c207c9496d0cd9
Michael Uftring <michael.uftring@gmail.com>
2018-07-14 20:49:04 -0400

    Homework 4: analysis of 2016 ANES data (in R, begin)

---
+++ exercises/homework-04/analysis.R
@@ -0,0 +1,10 @@
+# Michael Uftring
+# Indiana University
+# V506 - Statistical Analysis, Summer 2018
+# Homework 4 : analysis of Annual National Election Survey (ANES) data
+# --------------------------------------------------
+
+library(readr)
+
+anes <- read_csv("anes_2016.csv.xls")
+View(anes)
